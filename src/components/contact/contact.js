e4b9570344464229d83dfdc4a116fd5df9be344a
MignonGakuba <36228508+MignonGakuba@users.noreply.github.com>
2022-07-28 22:56:11 +0200

    Update contact.js
    Finishned first version of the section contact

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -23,9 +23,9 @@ const Contact = ()  => {
                 <a href="#">your.name@example.com</a>
             </div>
         </div>
-      <div class="col-lg-8">
+      <div class="col-lg-8 mt-6">
         <form class="contact-form validate-form" id="contact-form">
-            <div class="row  mt-4">
+            <div class="row st-4">
                 <div class="form-group col-sm-6 container">
                     <input name="name" id="name" type="text" class="form-control bordered" placeholder="Name"/>
                 </div>
@@ -33,12 +33,12 @@ const Contact = ()  => {
                     <input name="email" id="email" type="text" class="form-control bordered" placeholder="E-mail address"/>
                 </div>
             </div>
-            <div class="form-group">
+            <div class="form-group mt-4">
                 <textarea name="message" id="message" cols="30" rows="7" class="form-control bordered" placeholder="Message"></textarea>
             </div>
             <div class="row">
-            <div class="col-md-4 col-sm-6">
-                    <button class="btn btn-filled btn-primary btn-block">Send it <i class="i-after ti-arrow-right"></i></button>
+            <div class="col-lg-1 mt-2 ">
+                    <button class="btn btn-filled btn-primary btn-block">Confirm<i class="i-after ti-arrow-right"></i></button>
                 </div>
             </div>
         </form>
