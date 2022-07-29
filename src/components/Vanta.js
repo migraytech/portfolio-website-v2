88d4cf8876fb13616a9f29a639776d65a6f33d2c
MignonGakuba <36228508+MignonGakuba@users.noreply.github.com>
2022-07-29 09:08:55 +0200

    test

--- src/components/Vanta.js
+++ src/components/Vanta.js
@@ -1,37 +1,37 @@
 
-import React from 'react'
-import * as THREE from 'three'
-import FOG from '../vanta.globe.min.js';
+// import React from 'react'
+// import * as THREE from 'three'
+// import FOG from '../vanta.globe.min.js';
 
-class Vanta extends React.Component {
-    constructor() {
-      super()
-      this.vantaRef = React.createRef()
-    }
-    componentDidMount() {
-      this.vantaEffect = FOG({
-        el: this.vantaRef.current,
-        THREE: THREE,
-        mouseControls: true,
-        touchControls: true,
-        gyroControls: false,
-        minHeight: 200.00,
-        minWidth: 200.00,
-        highlightColor: 0x628e4d,
-        midtoneColor: 0xbb704f,
-        lowlightColor: 0xd41585
-      });
-    }
-    componentWillUnmount() {
-      if (this.vantaEffect) {
-        this.vantaEffect.destroy()
-      }
-    }
-    render() {
-      return <div id='vanta' ref={this.vantaRef}>
+// class Vanta extends React.Component {
+//     constructor() {
+//       super()
+//       this.vantaRef = React.createRef()
+//     }
+//     componentDidMount() {
+//       this.vantaEffect = FOG({
+//         el: this.vantaRef.current,
+//         THREE: THREE,
+//         mouseControls: true,
+//         touchControls: true,
+//         gyroControls: false,
+//         minHeight: 200.00,
+//         minWidth: 200.00,
+//         highlightColor: 0x628e4d,
+//         midtoneColor: 0xbb704f,
+//         lowlightColor: 0xd41585
+//       });
+//     }
+//     componentWillUnmount() {
+//       if (this.vantaEffect) {
+//         this.vantaEffect.destroy()
+//       }
+//     }
+//     render() {
+//       return <div id='vanta' ref={this.vantaRef}>
 
-      </div>
-    }
-  }
+//       </div>
+//     }
+//   }
   
-export default Vanta;
+// export default Vanta;
