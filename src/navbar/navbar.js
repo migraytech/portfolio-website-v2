3601eb625da84c966bc6a324216984de1789b32b
MignonGakuba <36228508+MignonGakuba@users.noreply.github.com>
2022-08-01 23:47:55 +0200

    Create a todo list and added footer class in the component package
    Setup a TODO list on the main App class

--- src/navbar/navbar.js
+++ src/navbar/navbar.js
@@ -7,7 +7,6 @@ const NavBar  = () =>{
     <nav class="navbar navbar-expand-lg fixed-top navbarScroll">
         <div class="container">
             <a class="navbar-brand" href="#">MG</a>
-            <img/>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
