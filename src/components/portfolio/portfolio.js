e3df2a4f6433117efed5b18f946184d96c21d4b6
MignonGakuba <36228508+MignonGakuba@users.noreply.github.com>
2022-08-14 11:13:53 +0200

    Update portfolio.js

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -52,7 +52,7 @@ const Portfolio = ()  => {
 
                         <div class="card-body">
                             <h4 class="card-title"> Youtube_Project</h4>
-                            <p class="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                            <p class="card-text">Created a  youtube project with Angular and SpringBoot.</p>
                             <div class="text-center">
                                 <a href="#" class=" btn btn-dark btn btn-success">Link</a>
                             </div>
